f66f7a99a67f7bc042c52ecc2fde50689275811a
GabrielGMenezes <gabgomen2@hotmail.com>
2023-04-08 12:27:28 -0300

    NovoControllerCliente

---
+++ src/main/java/br/com/lojavirtual/lojinha/controller/ClienteDTO.java
@@ -0,0 +1,12 @@
+package br.com.lojavirtual.lojinha.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteDTO {
+    private String teste;
+}
---
+++ src/main/java/br/com/lojavirtual/lojinha/service/ClienteService.java
@@ -0,0 +1,14 @@
+package br.com.lojavirtual.lojinha.service;
+
+import br.com.lojavirtual.lojinha.controller.ClienteDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    public List<ClienteDTO> obterListaClientes() {
+        return null;
+    }
+}
